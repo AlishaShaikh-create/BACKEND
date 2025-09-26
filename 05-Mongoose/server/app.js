833bbd2ccb4612b7ca24410c84f4fd4fdae0c3a4
AlishaShaikh-create <alishashaikh186555@gmail.com>
2025-09-26 17:29:26 +0530

    express and mongoose:

--- 05-Mongoose/server/app.js
+++ 05-Mongoose/server/app.js
@@ -1,2 +1,9 @@
-import mongoose from "mongoose";
-import express from 'express';
+import express from 'express'
+const app = express();
+const port = 5000;
+app.get('/' ,(req,res)=>{
+     res.send("<h1>HELLO</h1>")
+});
+app.listen(port,()=>{
+    console.log(`server is running at the port ${port} `);
+})
